3e7633d921f088172b843da01cc0730327d1adfa
fede <nasellofederico499@gmail.com>
2023-08-05 15:21:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,87 @@
+const pantalla = document.querySelector(".pantalla");
+const btn = document.querySelectorAll(".btn");
+window.addEventListener()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const saludo = "hola mundo";
+//console.log(saludo);
+//const chau = "adios";
+//console.log(chau);
+//
+//const horarios = ["15hs", "16hs", "20hs"];
+//console.log(horarios);
+//
+//const auto = {
+//  colores: ["azul", "verde", "negro"],
+//  tamanio: "2mts",
+//  autonomia: 55,
+//  doblarIzquierda: (grados) => {
+//    console.log(`doblar a la izquierda ${grados} falopa`);
+//  },
+//  acelerar: () => {
+//    console.log("acelerando");
+//  },
+//};
+//console.log({ auto });
+//const paredon = true;
+//if (paredon) {
+//  auto.doblarIzquierda(90);
+//} else {
+//  auto.acelerar();
+//}
+//const suma = (a, b) => {
+//  return a + b;
+//};
+//console.log(suma(5, 8));
+
+// pasos para realizar la calculadora 
+// escuchar clicks del navegador e identificar en que boton se hizo click consologueando el valor del mismo . 
+//addeventlistener mirar videos ,y docu
+
+
